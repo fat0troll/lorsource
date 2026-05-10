a302e15ac1490944df2c9bd4be45407a49445e46
Maxim Valyanskiy <max.valjanski@gmail.com>
2026-05-10 11:21:41 +0300

    Модернизация add-form.js: переход на современные конструкции JavaScript
    var → const/let, стрелочные функции, template literals, spread вместо $.extend,
    forEach вместо $.each, optional chaining для hcaptcha.reset, Object.fromEntries
    в getCookie, e.preventDefault() в beforeunload, константа UNSAVED_WARNING,
    dot notation для свойств объекта

--- src/main/webapp/js/add-form.js
+++ src/main/webapp/js/add-form.js
@@ -13,11 +13,13 @@
  *    limitations under the License.
  */
 
+const UNSAVED_WARNING = "Вы что-то напечатали в форме. Все введенные данные будут потеряны при закрытии страницы.";
+
 window.setupFormWithSpinner = function(options) {
-  function startSpinner($form) {
+  const startSpinner = ($form) => {
     const spinner = $("<i class='icon-spin spinner' style='margin-left: 0.5em'>");
     $form.find(".form-actions .btn").last().after(spinner);
-  }
+  };
 
   $script.ready("plugins", function() {
     $(function() {
@@ -25,12 +27,13 @@ window.setupFormWithSpinner = function(options) {
       const $textarea = $(options.textareaSelector);
       let submitted = false;
 
-      function warnOnUnload(e) {
+      const warnOnUnload = (e) => {
         if ($textarea.val() !== '' && !submitted) {
-          e.returnValue = "Вы что-то напечатали в форме. Все введенные данные будут потеряны при закрытии страницы.";
+          e.preventDefault();
+          e.returnValue = UNSAVED_WARNING;
           return e.returnValue;
         }
-      }
+      };
 
       window.addEventListener('beforeunload', warnOnUnload);
 
@@ -39,8 +42,9 @@ window.setupFormWithSpinner = function(options) {
         clickedButton = this;
       });
 
-      const validateOpts = $.extend({}, options.validateOptions, {
-        submitHandler: function(form) {
+      const validateOpts = {
+        ...options.validateOptions,
+        submitHandler: (form) => {
           if (submitted) { return; }
           submitted = true;
           window.removeEventListener('beforeunload', warnOnUnload);
@@ -55,7 +59,7 @@ window.setupFormWithSpinner = function(options) {
           $form.find(".form-actions button").prop("disabled", true);
           form.submit();
         }
-      });
+      };
 
       $form.validate(validateOpts);
     });
@@ -70,277 +74,281 @@ $script.ready(['jquery', 'hljs'], function() {
   }
 
   $(function() {
-  var commentForm = $("#commentForm");
-  if (!commentForm.length) {
-    return;
-  }
-
-  function getCookie(name) {
-    var match = document.cookie.match(new RegExp('(^|; )' + name + '=([^;]*)'));
-    return match ? match[2] : null;
-  }
-
-  function startSpinner($form) {
-    var spinner = $("<i class='icon-spin spinner' style='margin-left: 0.5em'>");
-    $form.find(".form-actions .btn").last().after(spinner);
-  }
-
-  function stopSpinner($form) {
-    $form.find(".spinner").remove();
-  }
-
-  function clearErrors($form) {
-    $form.find("div[error]").remove();
-  }
-
-  function scrollToPreview($preview) {
-    $preview.show();
-
-    var scrollTop = $(window).scrollTop();
-    var viewportBottom = scrollTop + window.innerHeight;
-    var previewTop = $preview.offset().top;
-
-    if (previewTop < scrollTop || previewTop > viewportBottom) {
-      $('html,body').animate({scrollTop: previewTop - window.innerHeight / 3}, 500);
-    }
-  }
-
-  commentForm.append($('<div id="commentPreview">').hide());
-  var commentPreview = $('#commentPreview');
-  var commentFormContainer = commentForm.parent();
-
-  var csrf = (getCookie("CSRF_TOKEN") || '').replace(/(^")|("$)/g, "");
-
-  var captchaLoaded = false;
-  var captchaRendered = false;
-
-  function loadCaptcha() {
-    var lazyCaptcha = commentFormContainer.find('[data-lazy-captcha]')[0];
-    if (!lazyCaptcha || captchaLoaded) {
+    const commentForm = $("#commentForm");
+    if (!commentForm.length) {
       return;
     }
 
-    captchaLoaded = true;
-    var sitekey = lazyCaptcha.getAttribute('data-sitekey');
-    var script = document.createElement('script');
-    script.src = 'https://js.hcaptcha.com/1/api.js?render=explicit';
-    script.async = true;
-    script.onload = function() {
-      if (typeof hcaptcha !== 'undefined' && !captchaRendered) {
-        hcaptcha.render(lazyCaptcha, {sitekey: sitekey});
-        captchaRendered = true;
-        lazyCaptcha.removeAttribute('data-lazy-captcha');
+    const getCookie = (name) => {
+      const cookies = Object.fromEntries(
+        document.cookie.split('; ').map(c => {
+          const [key, ...val] = c.split('=');
+          return [key, val.join('=')];
+        })
+      );
+      return cookies[name] ?? null;
+    };
+
+    const startSpinner = ($form) => {
+      const spinner = $("<i class='icon-spin spinner' style='margin-left: 0.5em'>");
+      $form.find(".form-actions .btn").last().after(spinner);
+    };
+
+    const stopSpinner = ($form) => {
+      $form.find(".spinner").remove();
+    };
+
+    const clearErrors = ($form) => {
+      $form.find("div[error]").remove();
+    };
+
+    const scrollToPreview = ($preview) => {
+      $preview.show();
+
+      const scrollTop = $(window).scrollTop();
+      const viewportBottom = scrollTop + window.innerHeight;
+      const previewTop = $preview.offset().top;
+
+      if (previewTop < scrollTop || previewTop > viewportBottom) {
+        $('html,body').animate({scrollTop: previewTop - window.innerHeight / 3}, 500);
       }
     };
-    document.head.appendChild(script);
-  }
 
-  function resetCaptcha() {
-    if (captchaRendered && typeof hcaptcha !== 'undefined') {
-      try { hcaptcha.reset(); } catch(e) { /* ignore */ }
-    }
-  }
+    commentForm.append($('<div id="commentPreview">').hide());
+    const commentPreview = $('#commentPreview');
+    const commentFormContainer = commentForm.parent();
 
-  function updateCsrf() {
-    if (csrf) {
-      $("input[name='csrf']", commentForm).val(csrf);
-    }
-  }
+    const csrf = (getCookie("CSRF_TOKEN") || '').replace(/(^")|("$)/g, "");
 
-  function updateAuthorReadonlyNote(authorReadonly) {
-    $('#author-readonly-note').text(
-      authorReadonly
-        ? "Внимание! Вы отвечаете на комментарий, автора которого не может создавать новые комментарии в этом топике."
-        : ""
-    );
-  }
+    let captchaLoaded = false;
+    let captchaRendered = false;
 
-  var REPLY_TYPE = 1;
-  var TOPIC_TYPE = 0;
+    const loadCaptcha = () => {
+      const lazyCaptcha = commentFormContainer.find('[data-lazy-captcha]')[0];
+      if (!lazyCaptcha || captchaLoaded) {
+        return;
+      }
 
-  function moveAndShowForm(selector, replyToValue) {
-    var replyTo = $("input[name='replyto']", commentFormContainer);
-    if (replyTo.val() !== String(replyToValue)) {
-      commentFormContainer.hide();
-    }
-
-    if (commentFormContainer.is(':hidden')) {
-      var reply = $('div.reply', $('div.msg_body', $(selector)));
-      reply.after(commentFormContainer);
-      replyTo.val(replyToValue);
-      loadCaptcha();
-      commentFormContainer.slideDown('slow', function() {
-        var formTop = commentFormContainer.offset().top;
-        var formHeight = commentFormContainer.outerHeight();
-        var viewportHeight = window.innerHeight;
-
-        var formBottom = formTop + formHeight;
-        var currentScrollTop = $(window).scrollTop();
-        var currentViewportBottom = currentScrollTop + viewportHeight;
-
-        var needsScroll;
-
-        if (formHeight <= viewportHeight) {
-          needsScroll = formTop < currentScrollTop || formBottom + 32 > currentViewportBottom;
-        } else {
-          var msgTop = $("#msg").offset().top;
-          needsScroll = msgTop < currentScrollTop || msgTop > currentViewportBottom;
+      captchaLoaded = true;
+      const sitekey = lazyCaptcha.getAttribute('data-sitekey');
+      const script = document.createElement('script');
+      script.src = 'https://js.hcaptcha.com/1/api.js?render=explicit';
+      script.async = true;
+      script.onload = () => {
+        if (typeof hcaptcha !== 'undefined' && !captchaRendered) {
+          hcaptcha.render(lazyCaptcha, {sitekey: sitekey});
+          captchaRendered = true;
+          lazyCaptcha.removeAttribute('data-lazy-captcha');
         }
+      };
+      document.head.appendChild(script);
+    };
 
-        if (needsScroll) {
-          var targetScrollTop;
+    const resetCaptcha = () => {
+      if (captchaRendered && typeof hcaptcha !== 'undefined') {
+        hcaptcha.reset?.();
+      }
+    };
+
+    const updateCsrf = () => {
+      if (csrf) {
+        $("input[name='csrf']", commentForm).val(csrf);
+      }
+    };
+
+    const updateAuthorReadonlyNote = (authorReadonly) => {
+      $('#author-readonly-note').text(
+        authorReadonly
+          ? "Внимание! Вы отвечаете на комментарий, автора которого не может создавать новые комментарии в этом топике."
+          : ""
+      );
+    };
+
+    const REPLY_TYPE = 1;
+    const TOPIC_TYPE = 0;
+
+    const moveAndShowForm = (selector, replyToValue) => {
+      const replyTo = $("input[name='replyto']", commentFormContainer);
+      if (replyTo.val() !== String(replyToValue)) {
+        commentFormContainer.hide();
+      }
+
+      if (commentFormContainer.is(':hidden')) {
+        const reply = $('div.reply', $('div.msg_body', $(selector)));
+        reply.after(commentFormContainer);
+        replyTo.val(replyToValue);
+        loadCaptcha();
+        commentFormContainer.slideDown('slow', () => {
+          const formTop = commentFormContainer.offset().top;
+          const formHeight = commentFormContainer.outerHeight();
+          const viewportHeight = window.innerHeight;
+
+          const formBottom = formTop + formHeight;
+          const currentScrollTop = $(window).scrollTop();
+          const currentViewportBottom = currentScrollTop + viewportHeight;
+
+          let needsScroll;
+
           if (formHeight <= viewportHeight) {
-            targetScrollTop = formBottom - viewportHeight + 32;
+            needsScroll = formTop < currentScrollTop || formBottom + 32 > currentViewportBottom;
           } else {
-            targetScrollTop = formTop - 16;
+            const msgTop = $("#msg").offset().top;
+            needsScroll = msgTop < currentScrollTop || msgTop > currentViewportBottom;
           }
-          targetScrollTop = Math.max(0, targetScrollTop);
 
-          $('html,body').animate({scrollTop: targetScrollTop}, 300, function() {
+          if (needsScroll) {
+            let targetScrollTop;
+            if (formHeight <= viewportHeight) {
+              targetScrollTop = formBottom - viewportHeight + 32;
+            } else {
+              targetScrollTop = formTop - 16;
+            }
+            targetScrollTop = Math.max(0, targetScrollTop);
+
+            $('html,body').animate({scrollTop: targetScrollTop}, 300, () => {
+              $("#msg").focus();
+            });
+          } else {
             $("#msg").focus();
-          });
-        } else {
-          $("#msg").focus();
-        }
-      });
-    } else {
-      commentFormContainer.slideUp('slow');
-    }
-  }
-
-  function toggleCommentForm(type, id, authorReadonly) {
-    updateCsrf();
-    updateAuthorReadonlyNote(authorReadonly);
-
-    if (type === REPLY_TYPE) {
-      moveAndShowForm('#comment-' + id, id);
-    } else if (type === TOPIC_TYPE) {
-      var topicId = $("input[name='topic']", commentFormContainer).val();
-      moveAndShowForm('#topic-' + topicId, 0);
-    }
-  }
-
-  $('div.reply').each(function() {
-    var container = this;
-
-    $('a[href^="comment-message.jsp"]', container).on("click", function(e) {
-      e.preventDefault();
-      toggleCommentForm(TOPIC_TYPE, 0, false);
-    });
-
-    var lnk = $('a[href^="add_comment.jsp"]', container);
-    if (lnk.length > 0) {
-      var ids = lnk.attr('href').match(/\d+/g);
-      var commentId = ids[1];
-      lnk.on("click", function(e) {
-        e.preventDefault();
-        toggleCommentForm(REPLY_TYPE, commentId, lnk.attr('data-author-readonly') === "true");
-      });
-    }
-  });
-
-  function warnOnUnloadComment(e) {
-    if ($("#msg").val() !== '' && !commentFormContainer.is(":hidden")) {
-      e.returnValue = "Вы что-то напечатали в форме. Все введенные данные будут потеряны при закрытии страницы.";
-      return e.returnValue;
-    }
-  }
-
-  window.addEventListener('beforeunload', warnOnUnloadComment);
-
-  commentForm.on("reset", function() {
-    commentFormContainer.slideUp('slow');
-    commentPreview.hide().empty();
-  });
-
-  var previewButton = commentForm.find("button[name=preview]");
-  previewButton.attr("type", "button");
-
-  function displayPreview(data) {
-    commentPreview.html('<h2>Предпросмотр</h2>' + data['preview']);
-    $('pre code', commentPreview).each(function(_i, block) {
-      hljs.highlightBlock(block);
-    });
-
-    if (data['errors']) {
-      var errors = $('<div class="error">');
-      $.each(data['errors'], function(_idx, v) {
-        errors.append($("<span>").text(v));
-        errors.append($("<br>"));
-      });
-      commentPreview.prepend(errors);
-      resetCaptcha();
-    }
-
-    scrollToPreview(commentPreview);
-  }
-
-  function ajaxError(jqXHR, textStatus, errorThrown) {
-    commentPreview.empty().append(
-      $('<div class="error">')
-        .text("Не удалось выполнить запрос, попробуйте повторить еще раз. " + errorThrown)
-    );
-    resetCaptcha();
-    scrollToPreview(commentPreview);
-  }
-
-  previewButton.on("click", function() {
-    previewButton.prop("disabled", true);
-    var form = commentForm.serialize() + "&preview=preview";
-
-    startSpinner(commentForm);
-    clearErrors(commentForm);
-
-    $.ajax({
-      method: "POST",
-      url: "/add_comment_ajax",
-      data: form,
-      timeout: 10000
-    }).always(function() {
-      previewButton.prop("disabled", false);
-      stopSpinner(commentForm);
-    }).fail(ajaxError).done(displayPreview);
-  });
-
-  var submitInProcess = false;
-
-  commentForm.on("submit", function(e) {
-    e.preventDefault();
-
-    if (submitInProcess) {
-      return;
-    }
-
-    submitInProcess = true;
-    window._commentSubmitting = true;
-    window.removeEventListener('beforeunload', warnOnUnloadComment);
-
-    var form = commentForm.serialize();
-
-    startSpinner(commentForm);
-    clearErrors(commentForm);
-
-    $.ajax({
-      method: "POST",
-      url: "/add_comment_ajax",
-      data: form,
-      timeout: 30000
-    }).always(function() {
-      submitInProcess = false;
-      stopSpinner(commentForm);
-    }).fail(function() {
-      window._commentSubmitting = false;
-      window.addEventListener('beforeunload', warnOnUnloadComment);
-      ajaxError.apply(this, arguments);
-    }).done(function(data) {
-      if (data['url']) {
-        window.location.href = data['url'];
+          }
+        });
       } else {
-        window._commentSubmitting = false;
-        window.addEventListener('beforeunload', warnOnUnloadComment);
-        displayPreview(data);
+        commentFormContainer.slideUp('slow');
+      }
+    };
+
+    const toggleCommentForm = (type, id, authorReadonly) => {
+      updateCsrf();
+      updateAuthorReadonlyNote(authorReadonly);
+
+      if (type === REPLY_TYPE) {
+        moveAndShowForm(`#comment-${id}`, id);
+      } else if (type === TOPIC_TYPE) {
+        const topicId = $("input[name='topic']", commentFormContainer).val();
+        moveAndShowForm(`#topic-${topicId}`, 0);
+      }
+    };
+
+    $('div.reply').each((_i, container) => {
+      $('a[href^="comment-message.jsp"]', container).on("click", (e) => {
+        e.preventDefault();
+        toggleCommentForm(TOPIC_TYPE, 0, false);
+      });
+
+      const lnk = $('a[href^="add_comment.jsp"]', container);
+      if (lnk.length > 0) {
+        const ids = lnk.attr('href').match(/\d+/g);
+        const commentId = ids[1];
+        lnk.on("click", (e) => {
+          e.preventDefault();
+          toggleCommentForm(REPLY_TYPE, commentId, lnk.attr('data-author-readonly') === "true");
+        });
       }
     });
-  });
+
+    const warnOnUnloadComment = (e) => {
+      if ($("#msg").val() !== '' && !commentFormContainer.is(":hidden")) {
+        e.preventDefault();
+        e.returnValue = UNSAVED_WARNING;
+        return e.returnValue;
+      }
+    };
+
+    window.addEventListener('beforeunload', warnOnUnloadComment);
+
+    commentForm.on("reset", () => {
+      commentFormContainer.slideUp('slow');
+      commentPreview.hide().empty();
+    });
+
+    const previewButton = commentForm.find("button[name=preview]");
+    previewButton.attr("type", "button");
+
+    const displayPreview = (data) => {
+      commentPreview.html('<h2>Предпросмотр</h2>' + data.preview);
+      $('pre code', commentPreview).each((_i, block) => {
+        hljs.highlightBlock(block);
+      });
+
+      if (data.errors) {
+        const errors = $('<div class="error">');
+        data.errors.forEach((v) => {
+          errors.append($("<span>").text(v));
+          errors.append($("<br>"));
+        });
+        commentPreview.prepend(errors);
+        resetCaptcha();
+      }
+
+      scrollToPreview(commentPreview);
+    };
+
+    const ajaxError = (jqXHR, textStatus, errorThrown) => {
+      commentPreview.empty().append(
+        $('<div class="error">')
+          .text(`Не удалось выполнить запрос, попробуйте повторить еще раз. ${errorThrown}`)
+      );
+      resetCaptcha();
+      scrollToPreview(commentPreview);
+    };
+
+    previewButton.on("click", () => {
+      previewButton.prop("disabled", true);
+      const form = commentForm.serialize() + "&preview=preview";
+
+      startSpinner(commentForm);
+      clearErrors(commentForm);
+
+      $.ajax({
+        method: "POST",
+        url: "/add_comment_ajax",
+        data: form,
+        timeout: 10000
+      }).always(() => {
+        previewButton.prop("disabled", false);
+        stopSpinner(commentForm);
+      }).fail(ajaxError).done(displayPreview);
+    });
+
+    let submitInProcess = false;
+
+    commentForm.on("submit", (e) => {
+      e.preventDefault();
+
+      if (submitInProcess) {
+        return;
+      }
+
+      submitInProcess = true;
+      window._commentSubmitting = true;
+      window.removeEventListener('beforeunload', warnOnUnloadComment);
+
+      const form = commentForm.serialize();
+
+      startSpinner(commentForm);
+      clearErrors(commentForm);
+
+      $.ajax({
+        method: "POST",
+        url: "/add_comment_ajax",
+        data: form,
+        timeout: 30000
+      }).always(() => {
+        submitInProcess = false;
+        stopSpinner(commentForm);
+      }).fail((jqXHR, textStatus, errorThrown) => {
+        window._commentSubmitting = false;
+        window.addEventListener('beforeunload', warnOnUnloadComment);
+        ajaxError(jqXHR, textStatus, errorThrown);
+      }).done((data) => {
+        if (data.url) {
+          window.location.href = data.url;
+        } else {
+          window._commentSubmitting = false;
+          window.addEventListener('beforeunload', warnOnUnloadComment);
+          displayPreview(data);
+        }
+      });
+    });
   });
 });
